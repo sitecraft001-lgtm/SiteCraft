7800250b2a7ed532b9dbf2029778d5755b34a284
sitecraft001-lgtm <sitecraft001@gmail.com>
2026-01-24 19:29:12 +0530

    Create snippets.js

---
+++ snippets.js
@@ -0,0 +1,15 @@
+const SNIPPETS = [
+  {
+    id: "hello",
+    title: "Hello World",
+    tags: ["basics", "print"],
+    code: `print("Hello, world!")`
+  },
+  {
+    id: "requests",
+    title: "GET Request",
+    tags: ["api", "http"],
+    code: `import requests
+print(requests.get("https://api.github.com").json())`
+  }
+];
