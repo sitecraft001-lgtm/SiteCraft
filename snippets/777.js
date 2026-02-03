950e423ce23f202024e36e7fa54158b625a33d59
sitecraft001-lgtm <sitecraft001@gmail.com>
2026-02-03 20:22:43 +0530

    Update 777.js

--- snippets/777.js
+++ snippets/777.js
@@ -3,5 +3,28 @@ export default {
   title: "Turtle Star",
   tags: ["777", "python"],
   code: `from turtle import *
-...`
+from colorsys import *
+
+tracer(20)
+b = "black"
+bgcolor(b)
+h = 0
+pensize(2)
+speed(0)
+
+for i in range(600):
+    h += 0.009
+    color(hsv_to_rgb(h,1,1),b)
+    begin_fill()
+    for j in range(5):
+        fd(100)
+        rt(100)
+        fd(100)
+        lt(100)
+        rt(360 / 5)
+    end_fill()
+    rt(2)
+    hideturtle()
+
+done()`
 };
